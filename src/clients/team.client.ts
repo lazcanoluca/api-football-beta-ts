87fff86c61295c113f26985ec35b97ec8827de49
lazcanoluca <lazcanoluca@gmail.com>
2023-12-20 17:26:12 -0300

    feat: add team search params

--- src/clients/team.client.ts
+++ src/clients/team.client.ts
@@ -2,7 +2,14 @@ import { BaseClient } from "../base_client";
 import { ENDPOINTS } from "../constants";
 import { Team } from "../models";
 
-type TeamRequestParams = {};
+type TeamRequestParams = {
+  country: string;
+  season: number;
+  league: number;
+  id: number;
+  search: string;
+  name: string;
+};
 
 export class TeamClient extends BaseClient {
   public async getTeams(params: Partial<TeamRequestParams>): Promise<Team[]> {
