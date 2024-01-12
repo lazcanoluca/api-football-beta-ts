fa109c445af8c8136075df1dc42df5f5516687c2
alendavies <alendavies10@gmail.com>
2024-01-12 14:56:04 -0300

    feat: allow new api

--- src/base_client.ts
+++ src/base_client.ts
@@ -2,84 +2,89 @@ import axios, { AxiosInstance } from "axios";
 
 import { ENDPOINTS } from "./constants";
 import {
-  AxiosCacheInstance,
-  buildWebStorage,
-  setupCache,
+    AxiosCacheInstance,
+    buildWebStorage,
+    setupCache,
 } from "axios-cache-interceptor";
 
 type Endpoint = (typeof ENDPOINTS)[keyof typeof ENDPOINTS];
 
 export type ApiFootballResponse<TEntity, TParams> = {
-  get: Endpoint;
-  parameters: TParams;
-  errors: [];
-  results: number;
-  response: TEntity[];
+    get: Endpoint;
+    parameters: TParams;
+    errors: [];
+    results: number;
+    response: TEntity[];
 };
 
 export type ClientConfig = {
-  rapid_api_key: string;
+    api_key: string;
+    base_url?: string;
+    base_host?: string;
 };
 
 export class BaseClient {
-  protected axiosInstance: AxiosInstance;
-  protected axiosInstanceWithCache: AxiosCacheInstance;
-  protected config: ClientConfig;
+    protected axiosInstance: AxiosInstance;
+    protected axiosInstanceWithCache: AxiosCacheInstance;
+    protected config: ClientConfig;
 
-  constructor(config: ClientConfig) {
-    this.config = config;
-    this.axiosInstance = axios.create({
-      method: "GET",
-      baseURL: "https://api-football-beta.p.rapidapi.com",
-      headers: {
-        "X-RapidAPI-Key": this.config.rapid_api_key,
-        "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
-      },
-    });
-    this.axiosInstanceWithCache = setupCache(this.axiosInstance, {
-      storage: buildWebStorage(localStorage),
-    });
-  }
-
-  protected async getEntityById<TEntity>(
-    endpoint: Endpoint,
-    id: number,
-    cache?: {
-      ttl?: number;
+    constructor(config: ClientConfig) {
+        this.config = config;
+        this.axiosInstance = axios.create({
+            method: "GET",
+            baseURL:
+                this.config.base_url ||
+                "https://api-football-beta.p.rapidapi.com",
+            headers: {
+                "X-RapidAPI-Key": this.config.api_key,
+                "X-RapidAPI-Host":
+                    this.config.base_host || "api-football-beta.p.rapidapi.com",
+            },
+        });
+        this.axiosInstanceWithCache = setupCache(this.axiosInstance, {
+            storage: buildWebStorage(localStorage),
+        });
     }
-  ): Promise<TEntity> {
-    return new Promise<TEntity>((resolve, reject) => {
-      this.axiosInstanceWithCache
-        .get<ApiFootballResponse<TEntity, { id: number }>>(endpoint, {
-          params: { id },
-          cache: cache ? cache : false,
-        })
-        .then((res) => {
-          if (res.data.results > 0) {
-            resolve(res.data.response[0]);
-          } else {
-            reject(); // TODO: custom exceptions handling module
-          }
-        })
-        .catch((err) => reject(err));
-    });
-  }
 
-  protected async getEntityList<TEntity, TParams extends {} = never>(
-    endpoint: Endpoint,
-    params: TParams,
-    cache?: {
-      ttl?: number;
+    protected async getEntityById<TEntity>(
+        endpoint: Endpoint,
+        id: number,
+        cache?: {
+            ttl?: number;
+        }
+    ): Promise<TEntity> {
+        return new Promise<TEntity>((resolve, reject) => {
+            this.axiosInstanceWithCache
+                .get<ApiFootballResponse<TEntity, { id: number }>>(endpoint, {
+                    params: { id },
+                    cache: cache ? cache : false,
+                })
+                .then((res) => {
+                    if (res.data.results > 0) {
+                        resolve(res.data.response[0]);
+                    } else {
+                        reject(); // TODO: custom exceptions handling module
+                    }
+                })
+                .catch((err) => reject(err));
+        });
     }
-  ): Promise<TEntity[]> {
-    return new Promise<TEntity[]>((resolve, reject) => {
-      this.axiosInstanceWithCache
-        .get<ApiFootballResponse<TEntity, TParams>>(endpoint, {
-          params: { ...params },
-          cache: cache ? cache : false,
-        })
-        .then((res) => resolve(res.data.response))
-        .catch((err) => reject(err));
-    });
-  }
+
+    protected async getEntityList<TEntity, TParams extends {} = never>(
+        endpoint: Endpoint,
+        params: TParams,
+        cache?: {
+            ttl?: number;
+        }
+    ): Promise<TEntity[]> {
+        return new Promise<TEntity[]>((resolve, reject) => {
+            this.axiosInstanceWithCache
+                .get<ApiFootballResponse<TEntity, TParams>>(endpoint, {
+                    params: { ...params },
+                    cache: cache ? cache : false,
+                })
+                .then((res) => resolve(res.data.response))
+                .catch((err) => reject(err));
+        });
+    }
 }
